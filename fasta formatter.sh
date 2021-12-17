a573ecca7ae70bf31981e78e19678d29f7a41f55
Bendemeurichy <48103261+Bendemeurichy@users.noreply.github.com>
2021-12-17 19:17:04 +0100

    first commit

---
+++ fasta formatter.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+#Ben De Meurichy 16/12/2021
+
+#standaard syntax error
+
+syntax() {
+
+    echo "Syntaxis: format [-w breedte] [-u|-l] FASTA" >&2
+
+}
+
+#opties
+
+u=0
+
+l=0
+
+w=80
+
+while getopts ":uw:l" opt; do
+
+    case $opt in
+
+        u)
+
+            u=1
+
+            ;;
+
+        l)
+
+            l=1
+
+            ;;
+
+        w)
+
+            if [[ $OPTARG =~ ^[+-]?[1-9][0-9]*$ ]];then
+
+                w=$OPTARG
+
+            else
+
+                syntax
+
+                exit 3
+
+            fi
+
+            ;;
+
+        \?)
+
+            syntax
+
+            exit 1
+
+    esac
+
+done
+
+shift $((OPTIND-1))
+
+#error handling
+
+if [[ $u -eq 1 && $l -eq 1 ]];then
+
+    syntax
+
+    exit 2
+
+fi
+
+if [[ ! $# -eq 1 ]] || [[ ! -f $1 || ! -r $1 ]];then
+
+    syntax
+
+    exit 4
+
+fi
+
+#verwerking tekst
+
+cat $1 |tr -d "[ \n]" | sed "s/\(>seq\)/\n\1/g"|sed "s/\(>seq[0-9]*\)/\1\n/g"|sed -r '/^\s*$/d'|sed "s/\(.\{$w\}\)/\1\n/g" >temp.txt
+
+if [ $u -eq 1 ];then
+    cat temp.txt |tr "a-z" "A-Z" >final.txt
+    sed -i "s/SEQ/seq/g" final.txt
+    cat final.txt
+    rm final.txt
+elif [ $l -eq 1 ];then
+    cat temp.txt |tr "A-Z" "a-z"
+else
+    cat temp.txt
+fi
+
+rm temp.txt
+
---
+++ olympischeAnimaties.sh
@@ -0,0 +1,177 @@
+#!/usr/bin/env bash
+#Ben De Meurichy 16/12/2021
+
+#standard syntax error
+
+syntax() {
+    echo "Syntax: track [-b <int>] [-h <char>] [-n] FILE FRAME" >&2
+}
+
+#round
+
+round() {
+  echo $(printf %.0f $(echo "($1 + 0.5)/1" | bc))
+}
+
+#line function
+
+repeat (){
+    if [[ $3 -lt 1 ]];then
+        echo -n "$1"
+    else
+        echo -n "$1"
+        for ((i=0 ; i<$3 ; i++));do
+            echo -n "$2"
+        done
+    fi
+}
+
+#options
+
+b=20
+h=$(echo "@")
+n=0
+
+while getopts ":b:h:n" opt; do
+    case $opt in
+        b)
+            if [[ $OPTARG =~ ^[+-]?[1-9][0-9]*$ ]];then
+                b=$((OPTARG+2))
+            else
+                syntax
+                exit 2
+            fi
+            ;;
+        h)
+            if [[ $OPTARG =~ ^[^0-9]$ ]]; then
+                h=$(echo "$OPTARG")
+            else
+                syntax
+                exit 3
+            fi
+            ;;
+        n)
+            n=1
+            ;;
+        \?)
+            syntax
+            exit 1
+    esac
+done
+
+shift $((OPTIND-1))
+
+#error handling
+
+if [[ $# -ne 2 ]];then
+    syntax
+    exit 1
+
+elif [[ ! -f $1 || ! -r $1 ]];then
+    syntax
+    exit 4
+
+elif [[ ! $2 =~ ^[+-]?[0-9][1-9]*$ ]] && [[ $2 -gt $(cat $1|wc -l) ]];then
+    syntax
+    exit 5
+
+fi
+
+#frame
+
+frame=$(echo $2)
+
+cat $1|tr "\t" " "| cut -d " " -f 1 --complement >atletes.txt
+runners=$(cat atletes.txt|head -1|wc -w)
+
+length=$(cat $1|tail -n 1|tr "\t" " "|cut -d " " -f 2)
+
+#lichaam
+houding=$((frame%3))
+
+if [[ $houding -eq 0 ]];then
+    hoofd=$(printf " $h/")
+    lichaam=$(printf "/| ")
+    benen=$(printf "/ \\")
+
+elif [[ $houding -eq 1 ]];then
+    hoofd=$(printf " $h ")
+    lichaam=$(echo "-|-")
+    benen=$(printf " | ")
+
+elif [[ $houding -eq 2 ]];then
+    hoofd=$(printf "\\$h ")
+    lichaam=$(printf " |\\")
+    benen=$(printf "/ \\")
+fi
+
+#print
+
+echo "Runners: $runners, Length: $length, Frame: $frame"
+repeat "+" "---+" "$b"
+echo""
+
+for ((i=1 ; i<=$runners ; i++));do
+
+
+afstand=$(cat atletes.txt|sed -n "$frame"p|cut -d " " -f $i)
+pos=$(echo "($b - 1)*($afstand/$length) "|bc -l)
+blok=$(round $pos)
+
+if [[ blok -eq 0 ]];then
+
+    repeat "|$hoofd|" "   |" "$b-2"
+    echo "###|"
+    repeat "|$lichaam|" "   |" "$b-2"
+    if [[ $n -eq 1 ]];then
+        echo "#"$i"#|"
+    else
+        echo "###|"
+    fi
+    repeat "|$benen|" "   |" "$b-2"
+    echo "###|"
+
+elif [[ blok -eq $((b-1)) ]];then
+
+    repeat "|###|" "   |" "$b-2"
+    echo "$hoofd|"
+    
+    if [[ $n -eq 1 ]];then
+        repeat "|#"$i"#|" "   |" "$b-2"
+    else
+        repeat "|###|" "   |" "$b-2"
+    fi
+    repeat "|###|" "   |" "$b-2"
+    echo "$benen|"
+
+
+else
+    
+    repeat "|###|" "   |" "$blok-1"
+    echo -n "$hoofd|"
+    repeat "" "   |" "$b-$blok-2"
+    echo "###|"
+    if [[ $n -eq 1 ]];then
+        repeat "|#"$i"#|" "   |" "$blok-1"
+            echo -n "$lichaam|"
+        repeat "" "   |" "$b-$blok-2"
+        echo "#"$i"#|"
+    else
+        repeat "|###|" "   |" "$blok-1"
+        echo -n "$lichaam|"
+        repeat "" "   |" "$b-$blok-2"
+        echo "###|"
+    fi 
+
+    repeat "|###|" "   |" "$blok-1"
+    echo -n "$benen|"
+    repeat "" "   |" "$b-$blok-2"
+    echo "###|"
+   
+
+fi
+repeat "+" "---+" "$b"
+
+done
+
+rm atletes.txt
