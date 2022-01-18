812cb1402d7c3a563fc82c60cd9f29a696d4e7ac
BenDM <ben.demeurichy@gmail.com>
2022-01-18 13:15:42 +0100

    added common commands

---
+++ commonly_used/common_commands.sh
@@ -0,0 +1,19 @@
+sed -i "s/regex/subst/g" inputfile #overwrite input file
+
+echo -n "text" #echo without newline
+
+egrep -v "regex" #grep lines not matching regex
+
+n=$(printf %02d $n) #add leading zeros if var is not 2char wide
+
+sort -o inputfile outputfile #sort whitout writing output to stdout
+
+sed -i "1,/regex/!d" inputfile #delete lines untill pattern 
+
+sed -n "linenumber"p inputfile #print out llinenumber
+
+dirname var #the name of the directory of the var
+
+basenmae var #filename of the var
+
+pwd #name of the current working directory
